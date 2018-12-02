e3f12559e99a5417f91c98128c0f310c5abef34e
Lukasz Janyst <lukasz@jany.st>
2018-12-02 19:44:44 +0100

    ui: Clean up react overhead

---
+++ ui/src/components/ReefApp.js
@@ -0,0 +1,16 @@
+
+import React, { Component } from 'react';
+
+class ReefApp extends Component {
+  render() {
+    return (
+      <div className="App">
+          <p>
+            Hello World!
+          </p>
+      </div>
+    );
+  }
+}
+
+export default ReefApp;
