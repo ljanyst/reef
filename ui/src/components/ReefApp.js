f728c9d890a52eebf3998d2a51c35b3d8fa4f070
Lukasz Janyst <lukasz@jany.st>
2018-12-02 19:47:04 +0100

    dist: Add the LICENSE file and headers

--- ui/src/components/ReefApp.js
+++ ui/src/components/ReefApp.js
@@ -1,3 +1,9 @@
+//------------------------------------------------------------------------------
+// Author: Lukasz Janyst <lukasz@jany.st>
+// Date: 02.12.2018
+//
+// Licensed under the GPL 3 License, see the LICENSE file for details.
+//------------------------------------------------------------------------------
 
 import React, { Component } from 'react';
 
--- ui/src/index.js
+++ ui/src/index.js
@@ -1,3 +1,10 @@
+//------------------------------------------------------------------------------
+// Author: Lukasz Janyst <lukasz@jany.st>
+// Date: 02.12.2018
+//
+// Licensed under the GPL 3 License, see the LICENSE file for details.
+//------------------------------------------------------------------------------
+
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
